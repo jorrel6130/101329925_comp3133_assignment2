68ab219ff9bcc244dc5b15061ad8f9957d55dd49
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-06 02:13:02 -0400

    Established routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { UpdateemployeeComponent } from './updateemployee/updateemployee.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+    {
+        path: 'employee',
+        component: EmployeelistComponent
+    },
+    {
+        path: 'employee/view/:id',
+        component: ViewemployeeComponent
+    },
+    {
+        path: 'employee/add',
+        component: AddemployeeComponent
+    },
+    {
+        path: 'employee/update/:id',
+        component: UpdateemployeeComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'employee',
+        pathMatch: 'full'
+    }
+];
