f5a94ba90901f99f4178b3b5362746afc9f67e0f
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-06 20:41:54 -0400

    Added interfaces

---
+++ src/app/models/employee.ts
@@ -0,0 +1,13 @@
+export interface Employee {
+    first_name: string;
+    last_name: string;
+    email: string;
+    gender: string;
+    designation: string;
+    salary: string;
+    date_of_joining: any;
+    department: string;
+    employee_photo: string;
+    created_at: any;
+    updated_at: any;
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    username: string;
+    email: string;
+    password: string;
+    created_at: any;
+    updated_at: any;
+}
