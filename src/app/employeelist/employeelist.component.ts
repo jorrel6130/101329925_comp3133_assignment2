590ef9c543eabfc77c13f289e747e779d098fc8e
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-07 19:19:18 -0400

    Added search by designation or department functionality

--- src/app/employeelist/employeelist.component.ts
+++ src/app/employeelist/employeelist.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import { GET_ALL } from '../graphql/graphql.queries';
 import { Employee } from '../models/employee';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-employeelist',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employeelist.component.html',
   styleUrl: './employeelist.component.css'
 })
@@ -14,22 +16,53 @@ export class EmployeelistComponent implements OnInit {
   employees: Employee[] = [];
   loading = true;
   error: any;
+  form: any;
+  desOrDep: any;
+  name: any;
 
-  constructor(private readonly apollo: Apollo) { }
+  constructor(private readonly apollo: Apollo, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.apollo
-      .watchQuery({
-        query: GET_ALL
-      }).valueChanges.subscribe((result: any) => {
-        this.employees = result.data?.getAll
-        this.loading = result.loading
-        this.error = result.error
-      })
+    this.form = this.formBuilder.group({
+      desOrDep: new FormControl(),
+      name: new FormControl()
+    })
+
+    this.desOrDep = this.route.snapshot.queryParamMap.get('desordep')
+    this.name = this.route.snapshot.queryParamMap.get('name')
+
+    if (this.name) {
+      this.apollo
+        .watchQuery({
+          query: gql`
+          query{
+            getAll: searchByDesOrDep(option: "${this.desOrDep}", query: "${this.name}") {
+              _id
+              first_name
+              last_name
+              designation
+              department
+            }
+          }
+        `
+        }).valueChanges.subscribe((result: any) => {
+          this.employees = result.data?.getAll
+          this.loading = result.loading
+          this.error = result.error
+        })
+    } else {
+      this.apollo
+        .watchQuery({
+          query: GET_ALL
+        }).valueChanges.subscribe((result: any) => {
+          this.employees = result.data?.getAll
+          this.loading = result.loading
+          this.error = result.error
+        })
+    }
   }
 
   deleteEmployee(id: any) {
-    console.log(id)
     this.apollo.mutate({
       mutation: gql`
                 mutation {
@@ -40,6 +73,35 @@ export class EmployeelistComponent implements OnInit {
       alert(`Employee ID ${id} successfully deleted`)
       window.location.reload()
     })
+  }
+
+  search() {
+    if (this.form?.valid) {
+      this.desOrDep = this.form.value.desOrDep
+      this.name = this.form.value.name
+      if (this.name) {
+        this.router.navigateByUrl(`/employee?desordep=${this.desOrDep}&name=${this.name}`)
+        this.apollo
+        .watchQuery({
+          query: gql`
+          query{
+            getAll: searchByDesOrDep(option: "${this.desOrDep}", query: "${this.name}") {
+              _id
+              first_name
+              last_name
+              designation
+              department
+            }
+          }
+        `
+        }).valueChanges.subscribe((result: any) => {
+          this.employees = result.data?.getAll
+          this.loading = result.loading
+          this.error = result.error
+        })
+      }
+      
+    }
   }
 
 }
