d31e1f5ba5ddb55fd0eedd94a5945b649f9daa43
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-07 17:30:19 -0400

    Added delete functionality

--- src/app/employeelist/employeelist.component.ts
+++ src/app/employeelist/employeelist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
+import { Apollo, gql } from 'apollo-angular';
 import { GET_ALL } from '../graphql/graphql.queries';
 import { Employee } from '../models/employee';
 
@@ -10,26 +10,36 @@ import { Employee } from '../models/employee';
   styleUrl: './employeelist.component.css'
 })
 export class EmployeelistComponent implements OnInit {
-  
+
   employees: Employee[] = [];
   loading = true;
   error: any;
-  
-  constructor(private readonly apollo: Apollo) {}
+
+  constructor(private readonly apollo: Apollo) { }
 
   ngOnInit(): void {
     this.apollo
-    .watchQuery({
-      query: GET_ALL
-    }).valueChanges.subscribe(( result: any) => {
-      this.employees = result.data?.getAll
-      this.loading = result.loading
-      this.error = result.error
-    })
+      .watchQuery({
+        query: GET_ALL
+      }).valueChanges.subscribe((result: any) => {
+        this.employees = result.data?.getAll
+        this.loading = result.loading
+        this.error = result.error
+      })
   }
 
   deleteEmployee(id: any) {
-    throw new Error('Method not implemented.');
+    console.log(id)
+    this.apollo.mutate({
+      mutation: gql`
+                mutation {
+                  deleteEmployee: delEmp(_id: "${id}")
+                }
+              `
+    }).subscribe((result: any) => {
+      alert(`Employee ID ${id} successfully deleted`)
+      window.location.reload()
+    })
   }
 
 }
