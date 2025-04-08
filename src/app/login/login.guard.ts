d232f03d106737821943ef31fb293492bb907678
Jorrel Tigbayan <jorrel6130@gmail.com>
2025-04-07 20:50:56 -0400

    Add login service

--- src/app/login/login.guard.ts
+++ src/app/login/login.guard.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): MaybeAsync<GuardResult> {
-    return true;
+    if (localStorage.getItem('token')) {
+      return true
+    }
+
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }})
+    
+    return false
+
   }
   
 }
